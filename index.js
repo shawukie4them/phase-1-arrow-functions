6f9230d0d487f7c64ad84fd18ff83d811a6b895e
shawukie4them <shawukiekawuki@gmail.com>
2024-07-01 13:46:34 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,14 @@
+//Exercise 1
+const divide = function(){
+    return 2000 / 100;
+};
+
+//Exercise 2
+const square = (num) =>{
+    return num * num;
+};
+
+//Exercise 3
+const add = (a, b) => {
+    return a + b;
+};
